428bbb85458d4a49eddf6e3b6038fe3dd6449edc
Maiglon Lubacheuski <Maiglon@Maiglons-MacBook-Air.local>
2017-05-24 00:08:10 -0300

    Iniciando com JWT.

---
+++ jwt.php
@@ -0,0 +1,27 @@
+<?php
+
+	// HEADER
+	$header = [
+		'alg' => 'HS256',
+		'typ' => 'JWT'
+	];
+
+	// PAYLOAD
+	$payload = [
+		'name' => 'Maiglon Lubacheuski',
+		'email' => 'maiglonl@gmail.com'
+	];
+
+	$header = json_encode($header);
+	$payload = json_encode($payload);
+
+	$header = base64_encode($header);
+	$payload = base64_encode($payload);
+
+	// SIGNATURE
+
+	$key = 'biwdb208rvb874s734v9uhkjznlw0s7z4098j';
+
+	$signature = hash_hmac('sha256', "$header.$payload", $key, true);
+	$signature = base64_encode($signature);
+?>
